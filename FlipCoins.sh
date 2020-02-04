c23ab4fce28a7c1a73b3437c340357a2bd065884
Lakshmikanth9036 <kanth9036.1997@gmail.com>
2020-02-04 09:55:27 +0530

    [Lakshmikanth]add. solved flipcoin problem using conditions and loops

---
+++ FlipCoins.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+
+echo "Enter the vlaue of n"
+read n
+h=0
+t=0
+while [ $n -gt 0 ]
+do
+if (( $(awk -v r=0.$(( RANDOM )) -v d=0.5 'BEGIN {print (r>d)}') ))
+then
+	(( h++ ))
+else
+	(( t++ ))
+fi
+(( n-- ))
+done
+echo $h $t
+headper=$(echo $h $t | awk '{ printf "%f", ($1*100)/($1+$2) }')
+echo $headper
+
+
