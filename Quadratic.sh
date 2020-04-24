b48bc7ea23431f3d8124284d036cc8e552d3b07a
Lakshmikanth9036 <59154987+Lakshmikanth9036@users.noreply.github.com>
2020-04-24 11:24:38 +0530

    Revert "Functional programs"

--- Quadratic.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-function quadratic(){
-delta=$(( ($2*$2)-(4*$1*$3) ))
-r1=$(echo $2 $delta $1 | awk '{print -$1+sqrt($2)/(2*$3)}')
-r2=$(echo $2 $delta $1 | awk '{print -$1-sqrt($2)/(2*$3)}')
-echo $r1 $r2
-}
-quadratic 2 4 1
